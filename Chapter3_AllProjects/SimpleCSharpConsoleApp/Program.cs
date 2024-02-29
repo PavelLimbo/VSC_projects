03f8067f6e462a83c69363f12913b6f59ae14982
PavelLimbo <ronanlev2004@mail.ru>
2024-02-29 08:56:26 +0300

    Created BasicConsoleIO project

---
+++ Chapter3_AllProjects/SimpleCSharpConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+for(int i = 0; i< args.Length; i++)
+{
+    System.Console.WriteLine("Argument: {0}", args[i]);
+}
